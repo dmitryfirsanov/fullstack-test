e295414353b92eaa607e286ab2ec74ee5ccdd2c5
Dmitry Firsanov <dmirtyfirsanov03@gmail.com>
2024-10-04 23:03:29 +0300

    [frontend/feat] add button component

---
+++ frontend/src/components/BaseButton.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Spinner from './Spinner.vue';
+
+defineProps<{
+  disabled?: boolean;
+  loading?: boolean;
+}>();
+</script>
+
+<template>
+  <button
+    :class="[
+      'button',
+      { 'button--loading': loading },
+      { 'button--disabled': disabled },
+    ]"
+    :disabled="disabled"
+  >
+    <slot v-if="!loading"></slot>
+    <Spinner
+      class="button__spinner"
+      v-else
+    />
+  </button>
+</template>
+
+<style>
+.button {
+  background: #4c8bf7;
+  font-size: 14px;
+  font-weight: 600;
+  border: 1px solid transparent;
+  padding: 8px 12px;
+  box-sizing: border-box;
+  color: #fff;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+.button--loading {
+  padding: 8px;
+}
+
+.button--disabled {
+  cursor: not-allowed;
+  background: #f2f3f4;
+  border-color: #d6d9dd;
+  color: #1a1a1a;
+}
+</style>
