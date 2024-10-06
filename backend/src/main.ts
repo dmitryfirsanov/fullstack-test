4b1cc50d234e8824a081613c0c9e8079261a5c1c
Dmitry Firsanov <dmirtyfirsanov03@gmail.com>
2024-10-06 18:04:09 +0300

    [backend/feat] initial backend setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend API')
+    .setDescription('The Backend API description')
+    .setVersion('1.0')
+    .addTag('backend')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:4173',
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
